008da70e123073e2d347a4e303191a1e0016279b
Pancho <panchovecino@gmail.com>
2017-06-20 20:10:14 -0300

    Empezar a trabajar con el Login

---
+++ ObligatorioP3/ObligatorioP3/Controllers/FinanciadorsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ObligatorioP3.Models;
+
+namespace ObligatorioP3.Controllers
+{
+    public class FinanciadorsController : Controller
+    {
+        private ObliEmprendimientosContext db = new ObliEmprendimientosContext();
+
+        // GET: Financiadors
+        public ActionResult Index()
+        {
+            return View(db.Usuarios.ToList());
+        }
+
+        // GET: Financiadors/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Financiador financiador = db.Usuarios.Find(id);
+            if (financiador == null)
+            {
+                return HttpNotFound();
+            }
+            return View(financiador);
+        }
+
+        // GET: Financiadors/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Financiadors/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Email,Password,Rol,Organizacion,MontoMax")] Financiador financiador)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Usuarios.Add(financiador);/*/mbox bv[]*/
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(financiador);
+        }
+
+        // GET: Financiadors/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Financiador financiador = db.Usuarios.Find(id);
+            if (financiador == null)
+            {
+                return HttpNotFound();
+            }
+            return View(financiador);
+        }
+
+        // POST: Financiadors/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Email,Password,Rol,Organizacion,MontoMax")] Financiador financiador)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(financiador).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(financiador);
+        }
+
+        // GET: Financiadors/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Financiador financiador = db.Usuarios.Find(id);
+            if (financiador == null)
+            {
+                return HttpNotFound();
+            }
+            return View(financiador);
+        }
+
+        // POST: Financiadors/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Financiador financiador = db.Usuarios.Find(id);
+            db.Usuarios.Remove(financiador);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
