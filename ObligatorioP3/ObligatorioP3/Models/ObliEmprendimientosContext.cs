3f0660232d0047b7eb0c55ddd0df94d7e765ea41
Natalia Larrieux <natalia.larrieux@modelit.xyz>
2017-06-28 13:14:35 -0300

    Remover tabla UsuarioViewModel

--- ObligatorioP3/ObligatorioP3/Models/ObliEmprendimientosContext.cs
+++ ObligatorioP3/ObligatorioP3/Models/ObliEmprendimientosContext.cs
@@ -12,8 +12,6 @@ namespace ObligatorioP3.Models
         public DbSet<Emprendimiento> Emprendimientos { get; set; }
         public DbSet<Usuario> Usuarios { get; set; }
 
-        public System.Data.Entity.DbSet<ObligatorioP3.ViewModels.UsuarioViewModel> UsuarioViewModels { get; set; }
-
         public ObliEmprendimientosContext() : base("obligatorio") { }
     }
 }
