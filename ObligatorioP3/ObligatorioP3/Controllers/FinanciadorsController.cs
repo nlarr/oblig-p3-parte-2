6adff1b66cbeff2c46d06c735322c15c991fcd74
Natalia Larrieux <natalia.larrieux@modelit.xyz>
2017-06-28 12:36:14 -0300

    Remover vistas y controllers que sobraban

--- ObligatorioP3/ObligatorioP3/Controllers/FinanciadorsController.cs
+++ ObligatorioP3/ObligatorioP3/Controllers/FinanciadorsController.cs
@@ -14,28 +14,11 @@ namespace ObligatorioP3.Controllers
     {
         private ObliEmprendimientosContext db = new ObliEmprendimientosContext();
 
-        //// GET: Financiadors/Details/5
-        //public ActionResult Details(int? id)
-        //{
-        //    if (id == null)
-        //    {
-        //        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-        //    }
-        //    Financiador financiador = db.Usuarios.Find(id);
-        //    if (financiador == null)
-        //    {
-        //        return HttpNotFound();
-        //    }
-        //    return View(financiador);
-        //}
-
         // GET: Financiadors/Create
         public ActionResult Create()
         {
             return View();
         }
-
-        
 
         // POST: Financiadors/Create
         // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
@@ -53,37 +36,6 @@ namespace ObligatorioP3.Controllers
 
             return View(financiador);
         }
-
-        //// GET: Financiadors/Edit/5
-        //public ActionResult Edit(int? id)
-        //{
-        //    if (id == null)
-        //    {
-        //        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-        //    }
-        //    Financiador financiador = db.Usuarios.Find(id);
-        //    if (financiador == null)
-        //    {
-        //        return HttpNotFound();
-        //    }
-        //    return View(financiador);
-        //}
-
-        //// POST: Financiadors/Edit/5
-        //// Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
-        //// más información vea http://go.microsoft.com/fwlink/?LinkId=317598.
-        //[HttpPost]
-        //[ValidateAntiForgeryToken]
-        //public ActionResult Edit([Bind(Include = "Id,Email,Password,Rol,Organizacion,MontoMax")] Financiador financiador)
-        //{
-        //    if (ModelState.IsValid)
-        //    {
-        //        db.Entry(financiador).State = EntityState.Modified;
-        //        db.SaveChanges();
-        //        return RedirectToAction("Index");
-        //    }
-        //    return View(financiador);
-        //}
 
         // Metodo autogenerado
         protected override void Dispose(bool disposing)
